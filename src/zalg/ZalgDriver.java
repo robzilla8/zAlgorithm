74e5fd333dfb65c4baf80d5c4e37a3008f155adb
Rob <rob@Robs-MacBook-Pro.local>
2019-09-10 15:27:27 -0600

    Added another condition I wasn't handling to the naive search; getting to the end of the string and having a match on the last character should still increment the count by 1.

--- src/zalg/ZalgDriver.java
+++ src/zalg/ZalgDriver.java
@@ -3,18 +3,25 @@ package zalg;
 public class ZalgDriver {
 
 	public static void main(String[] args) {
-		int count = naiveSearch("ABABABABC", 0, 2);
-		System.out.printf("Count = %d", count);
+		String s = "ABSBAJSLABSKDFBAABASBABSBAJSLABKABSBAK";
+		
+		for (int i = 0; i < s.length(); i++) {
+			int count = naiveSearch(s, 0, i);
+			System.out.printf("Index = %d || Count = %d%n", i, count);
+		}
 	}
-	
+
 	private static int naiveSearch(String s, int indexToStartOn, int currentIndex) {
 		int count = 0;
-		
+
 		if (s.charAt(indexToStartOn) == s.charAt(currentIndex) && currentIndex != s.length() - 1) {
 			// we have a match
 			count = naiveSearch(s, indexToStartOn+1, currentIndex+1) + 1;
+		} else if(s.charAt(indexToStartOn) == s.charAt(currentIndex) && currentIndex == s.length() - 1){
+			// end of string & a match!
+			count = 1;
 		} else {
-			// mismatch or end of string
+			// mismatch, no code needed
 		}
 		return count;
 	}
