756a3a136019cad4b9a54fbd5d9d03495005b5d6
Rob <rob@Robs-MacBook-Pro.local>
2019-09-11 11:26:41 -0600

    Did some refactoring, and am trying our algorithm on a test string, but getting some odd results. Debug time

--- src/zalg/ZalgDriver.java
+++ src/zalg/ZalgDriver.java
@@ -5,25 +5,27 @@ public class ZalgDriver {
 	public static void main(String[] args) {
 		String s = "ABSBAJSLABSKDFBAABASBABSBAJSLABKABSBAK";
 		
-		for (int i = 0; i < s.length(); i++) {
-			int count = naiveSearch(s, 0, i);
-			System.out.printf("Index = %d || Count = %d%n", i, count);
-		}
+//		for (int i = 0; i < s.length(); i++) {
+//			int count = naiveSearch(s, 0, i);
+//			System.out.printf("Index = %d || Count = %d%n", i, count);
+//		}
+		ZalgImp zalg = new ZalgImp("TEST$ASTESLGHSLTETEST");
+		//zalg.Zalg("TEST$ASTESLGHSLTETEST");
 	}
 
-	private static int naiveSearch(String s, int indexToStartOn, int currentIndex) {
-		int count = 0;
-
-		if (s.charAt(indexToStartOn) == s.charAt(currentIndex) && currentIndex != s.length() - 1) {
-			// we have a match
-			count = naiveSearch(s, indexToStartOn+1, currentIndex+1) + 1;
-		} else if(s.charAt(indexToStartOn) == s.charAt(currentIndex) && currentIndex == s.length() - 1){
-			// end of string & a match!
-			count = 1;
-		} else {
-			// mismatch, no code needed
-		}
-		return count;
-	}
+//	public static int naiveSearch(String s, int indexToStartOn, int currentIndex) {
+//		int count = 0;
+//
+//		if (s.charAt(indexToStartOn) == s.charAt(currentIndex) && currentIndex != s.length() - 1) {
+//			// we have a match
+//			count = naiveSearch(s, indexToStartOn+1, currentIndex+1) + 1;
+//		} else if(s.charAt(indexToStartOn) == s.charAt(currentIndex) && currentIndex == s.length() - 1){
+//			// end of string & a match!
+//			count = 1;
+//		} else {
+//			// mismatch, no code needed
+//		}
+//		return count;
+//	}
 
 }
--- src/zalg/ZalgImp.java
+++ src/zalg/ZalgImp.java
@@ -9,18 +9,25 @@ public class ZalgImp {//a class which runs our implementation of the Z-algorithm
 	int[] zcounts;//the total number of Z-counts for a Z-box
 	ZalgDriver driver = new ZalgDriver();
 	
-	public ZalgImp() {//the constructor initializes all the values to zero, representing the beginning of any given string.
+	public ZalgImp(String str) {//the constructor initializes all the values to zero, representing the beginning of any given string.
+		zcounts = new int[str.length()];
 		zcount=0;
 		leftValue=0;
 		rightValue=0;
 		zcounts[0]=zcount;
+		Zalg(str);
+		int counter = 0;
+		for (int z : zcounts) {
+			System.out.printf("Counter = %d, Z = %d%n", counter, z);
+			counter++;
+		}
 	}
 	
 	public void Zalg(String patternString) {//the actual class which controls our Z-values. 
 		zcounts= new int[patternString.length()];
 		if(patternString.charAt(0)==patternString.charAt(1)) {//this if statement takes into account the Z2 value, as well as accounts for if the string has multiple values that are similar.
 			zcount+=1;
-			zcount+=driver.naiveSearch(patternString,1,2);
+			zcount+=naiveSearch(patternString,1,2);
 			zcounts[1]=zcount;
 			leftValue=1;
 			rightValue=zcount;
@@ -28,9 +35,9 @@ public class ZalgImp {//a class which runs our implementation of the Z-algorithm
 		else {
 			zcounts[1]=0;
 		}
-		for(int i=2;i<=patternString.length();i++) {//iterates through the string, simply moving the current position we are looking for. 
+		for(int i=2;i<patternString.length();i++) {//iterates through the string, simply moving the current position we are looking for. 
 			if(i>rightValue) {//case one of the Z-algorithm, the current position is outside of the Z-box. We check from the beginning till we find a new string.
-				zcount=driver.naiveSearch(patternString,1,i);
+				zcount=naiveSearch(patternString,1,i);
 				zcounts[i]=zcount;
 				if(zcount>0) {
 					leftValue=i;
@@ -42,7 +49,7 @@ public class ZalgImp {//a class which runs our implementation of the Z-algorithm
 					zcounts[i]=zcounts[i-leftValue];
 				}
 				else if(zcounts[i-leftValue]>=(rightValue-i)) {
-					zcount=driver.naiveSearch(patternString,(rightValue-i),rightValue);
+					zcount=naiveSearch(patternString,(rightValue-i),rightValue);
 					zcounts[i]=zcount;
 					leftValue=i;
 					rightValue=(i+zcount+1);
@@ -52,5 +59,19 @@ public class ZalgImp {//a class which runs our implementation of the Z-algorithm
 		}
 	}
 	
-}
+	private static int naiveSearch(String s, int indexToStartOn, int currentIndex) {
+		int count = 0;
+
+		if (s.charAt(indexToStartOn) == s.charAt(currentIndex) && currentIndex != s.length() - 1) {
+			// we have a match
+			count = naiveSearch(s, indexToStartOn+1, currentIndex+1) + 1;
+		} else if(s.charAt(indexToStartOn) == s.charAt(currentIndex) && currentIndex == s.length() - 1){
+			// end of string & a match!
+			count = 1;
+		} else {
+			// mismatch, no code needed
+		}
+		return count;
+	}
+	
 }
