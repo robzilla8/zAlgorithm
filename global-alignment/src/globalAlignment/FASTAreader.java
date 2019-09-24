87d48082c54c8d46e01b4d97b41b662989130445
nardielloam <nardielloam@gmail.com>
2019-09-24 08:11:01 -0600

    Adds the global-alignment
    Takes in the global alignment algorithm and commits it to the repository for all of Comp Bio

---
+++ global-alignment/src/globalAlignment/FASTAreader.java
@@ -0,0 +1,5 @@
+package globalAlignment;
+
+public class FASTAreader {
+
+}
---
+++ global-alignment/src/globalAlignment/GlobAlignDriver.java
@@ -0,0 +1,10 @@
+package globalAlignment;
+
+public class GlobAlignDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
